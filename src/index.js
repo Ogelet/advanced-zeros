29af171b7f4a4a5ea3381b383b285db5576c369c
Svetlana Telego <camarezi@gmail.com>
2018-03-05 09:29:42 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ module.exports = function getZerosCount(number, base) {
   let factor = -1;
   let factors = [];
   let checkNumber = base;
+  let exp = 1;
   while(checkNumber%2 === 0){
     checkNumber = checkNumber/2;
   };
@@ -10,7 +11,6 @@ module.exports = function getZerosCount(number, base) {
       factors.push(i);
     }
   };
-  console.log('factors', factors);
   const lengthFactors = factors.length;
   if(lengthFactors === 1){
     factor = checkNumber;
@@ -19,7 +19,6 @@ module.exports = function getZerosCount(number, base) {
     factor = 2;
   };
   if(lengthFactors > 1){
-    console.log('Yes', factors);
       for(let i = lengthFactors - 1; i > 0; i--){
       let checkFactor = 0;
       for(let j = i - 1; j > -1; j--){
@@ -29,33 +28,27 @@ module.exports = function getZerosCount(number, base) {
       };
       if (checkFactor === 0){
         factor = factors[i];
-        break
-      };
+        break;
+      } else {
+        for (let x = 0; x < lengthFactors; x++){
+          for (let n = 2; n < 5; n++ )
+          if (Math.pow(factors[x],n) === factors[i] && exp===1){
+            factor = factors[x];
+            exp = n;            
+          }
+        }
+      }
     }
   };
   if(lengthFactors > 1 && factor === -1 ){
     factor = factors[0];
   }
-/*
-  let count = number / factor;
-  for(let i = factor; i <= number; i = i + factor){
-    if ((i / factor) % factor === 0){
-      count++;
-      let a = i / factor;
-      while((a/factor) % factor === 0){
-        count++;
-        a = a / factor;
-      };
-    };
-  };
-  */
- console.log('factor', factor);
  let count = 0;
  let x = factor;
  while(number/x >= 1){
    count = count + Math.floor(number / x);
    x = factor * x;
  }
- console.log('count', count);
+ count = Math.floor(count/exp);
   return count;
 }
